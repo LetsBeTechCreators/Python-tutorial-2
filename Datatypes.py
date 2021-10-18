1917fc5f17692e4be461704459be6725983cda70
Sayantan Das <89656888+LetsBeTechCreators@users.noreply.github.com>
2021-10-18 15:52:51 +0530

    Create Datatypes.py

---
+++ Datatypes.py
@@ -0,0 +1,19 @@
+# what are datatypes
+a = 10
+b = 10.0
+c = '''10'''
+# a = type(a)
+# b = type(b)
+# c = type(c)
+# print(a)
+# print(b)
+# print(c)
+
+print (a + float(c))#Typecasting
+
+# a = 2
+# b = 2
+# c = a+b
+
+# print(c)
+
